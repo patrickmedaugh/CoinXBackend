d4e27f3579e75c95c7d36459c88b730bbcc35f8a
patrickmedaugh <patrickmedaugh@gmail.com>
2016-12-07 14:30:25 -0700

    Backend in place and functional with cors. Needs more routes but after that onto Mongo integration.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express();
 import request from 'request';
+import cors from 'cors';
+app.use(cors());
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -8,25 +10,25 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
 
-app.get('/litecoin-btce', function(req, res) {
+app.get('/litecoin-btce', (req, res) => {
+    request("https://btc-e.com/api/3/ticker/ltc_btc", function (error, response, body) {
+      res.send(JSON.parse(body))
+    })
+});
+
+app.get('/bitcoin-btce', (req, res) => {
     request("https://btc-e.com/api/3/ticker/btc_usd", function (error, response, body) {
-      console.log(typeof body);
-      res.send(body)
+      res.send(JSON.parse(body))
     })
 });
 
-app.get('/bitcoin-btce', function(req, res) {
-    var thing;
-    /*
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => {
-      if (response.ok) {
-        thing = "at least this works";
-      }
+app.get('/dash-poloniex', (req, res) => {
+    request("https://poloniex.com/public?command=returnTradeHistory&currencyPair=BTC_DASH&depth=20", function (error, response, body) {
+      res.send(JSON.parse(body))
     })
-    */
-    res.send(JSON.stringify({newkidsontheblock: "sucking a lot of dick"}));
 });
 
-app.listen(port);
-console.log("Listening on port ", port);
+
+app.listen(port, () => {
+  console.log("Listening on port ", port);
+});
